830348af83c365a35e2edd5b41ca9b70ab668e10
BoyVOL <39579095+BoyVOL@users.noreply.github.com>
2021-11-06 19:41:47 +0900

    1

--- Source Code/ForceProjectors.cs
+++ Source Code/ForceProjectors.cs
@@ -24,3 +24,26 @@ public class GravityProjector : ForceProjector{
             return Normal*module;
         }
     }
+
+public class GravityRailProjector : ForceProjector{
+        Rail Rail;
+
+        float Potential;
+
+        public GravityRailProjector(Rail rail, float potential){
+            Rail = rail;
+            Potential = potential;
+        }
+
+        float Force(float r, float potential){
+            return potential/(r*r);
+        }
+        public override Vector2 GetAccelVector(ForceParams forceParams, float T){
+            Vector2 Pos = Rail.Interpolate(T).Position;
+            float R = Pos.DistanceTo(forceParams.Pos);
+            Vector2 Normal = (Pos - forceParams.Pos);
+            Normal.Normalized();
+            float module = Potential/(R*R);
+            return Normal*module;
+        }
+    }
