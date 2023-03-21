54cfa26c5499c9385058c6de93074598d8026cc0
BoyVOL <vladislav_limonov@mail.ru>
2023-03-21 17:27:48 +0900

    1

--- Attempt3/addons/OrbitalPhysics2D/ClassLib/ClientClasses/Nodes/PhysRailNode.cs
+++ Attempt3/addons/OrbitalPhysics2D/ClassLib/ClientClasses/Nodes/PhysRailNode.cs
@@ -3,8 +3,54 @@ using System;
 using System.Collections.Generic;
 
 public partial class PhysRailNode: Node2D{
+	
+	[Export]
+	public Color PredictionColor;
+
+	/// <summary>
+	/// List af all points that predict movement of this object for certain period of time
+	/// </summary>
+	/// <returns></returns>
+	public RailPointList PredictionRail;
+
+	/// <summary>
+	/// Ref to controlling object
+	/// </summary>
+	public PhysicsControlNode PhysNode = null;
+
+	
+	[Export]
+	public Color PhysRailColor;
+
+	/// <summary>
+	/// List of all simulation points of this object
+	/// </summary>
+	/// <typeparam name="PhysRail"></typeparam>
+	/// <returns></returns>
+	public RailPointList PhysRail;
 
 	public Collider Collider = null;
+
+	public PhysRailNode():base(){
+		PredictionRail = new RailPointList(this);
+		PhysRail = new RailPointList(this);
+	}
+
+	/// <summary>
+	/// Method for updating node pos according to simulation
+	/// </summary>
+	public void UpdatePos(){
+		Position = PhysRail[0].Position;
+	}
+
+	public void DrawPhys(){
+			Vector2[] Points = new Vector2[PhysRail.Count];
+			for (int i = 0; i < PhysRail.Count; i++)
+			{
+				Points[i] = PhysRail[i].Position-PhysRail[0].Position;
+			}
+			if(Points.Length > 1) DrawPolyline(Points,PhysRailColor,2);
+	}
 
 	public void DrawCollisions(){
 		if(Collider != null){
@@ -22,15 +68,6 @@ public partial class PhysRailNode: Node2D{
 			DrawArc(Vector2.Zero,Collider.Radius,0,(float)Math.PI*2,100,Collider.RadiusColor);
 		}
 	}
-	
-	[Export]
-	public Color PredictionColor;
-
-	/// <summary>
-	/// List af all points that predict movement of this object for certain period of time
-	/// </summary>
-	/// <returns></returns>
-	public RailPointList PredictionRail;
 
 	public void DrawPred(){
 			Vector2[] Points = new Vector2[PredictionRail.Count];
@@ -70,42 +107,5 @@ public partial class PhysRailNode: Node2D{
 		DrawCollisions();
 		DrawPred();
 		#endif
-	}
-
-	/// <summary>
-	/// Ref to controlling object
-	/// </summary>
-	public PhysicsControlNode PhysNode = null;
-
-	
-	[Export]
-	public Color PhysRailColor;
-
-	/// <summary>
-	/// List of all simulation points of this object
-	/// </summary>
-	/// <typeparam name="PhysRail"></typeparam>
-	/// <returns></returns>
-	public RailPointList PhysRail;
-
-	public PhysRailNode():base(){
-		PredictionRail = new RailPointList(this);
-		PhysRail = new RailPointList(this);
-	}
-
-	/// <summary>
-	/// Method for updating node pos according to simulation
-	/// </summary>
-	public void UpdatePos(){
-		Position = PhysRail[0].Position;
-	}
-
-	public void DrawPhys(){
-			Vector2[] Points = new Vector2[PhysRail.Count];
-			for (int i = 0; i < PhysRail.Count; i++)
-			{
-				Points[i] = PhysRail[i].Position-PhysRail[0].Position;
-			}
-			if(Points.Length > 1) DrawPolyline(Points,PhysRailColor,2);
 	}
 }
