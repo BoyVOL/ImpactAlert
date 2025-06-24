31ecde85c37ee652521710e41f0e0106d43d8796
BoyVOL <vladislav_limonov@mail.ru>
2025-06-24 10:54:42 +0900

    Добавил глобальные пути

--- Attempt3/addons/Rail2DAutomator/Rail2DAutomator.cs
+++ Attempt3/addons/Rail2DAutomator/Rail2DAutomator.cs
@@ -5,15 +5,16 @@ using System;
 [Tool]
 public partial class Rail2DAutomator : EditorPlugin
 {
+	String GlobalPath = "res://addons/Rail2DAutomator/";
 	public override void _EnterTree()
 	{
 		// Initialization of the plugin goes here.
+		AddCustomType("RailAutomator", "Node", GD.Load<Script>(GlobalPath+"Nodes/RailAutomator/RailAutomator.cs"),
+		GD.Load<Texture2D>(GlobalPath+"Nodes/RailAutomator//icon.png"));
+		AddCustomType("AutoRailNode", "Node2D", GD.Load<Script>(GlobalPath+"Nodes/AutoRailNode/AutoRailNode.cs"),
+		GD.Load<Texture2D>(GlobalPath+"Nodes/AutoRailNode//icon.png"));
+        AddAutoloadSingleton("RailAutomator", GlobalPath+"Scenes/AutomatorSingleton.tscn");
 		GD.Print("Rail2DAutomator loaded");
-		AddCustomType("RailAutomator", "Node", GD.Load<Script>("res://addons/Rail2DAutomator/Nodes/RailAutomator/RailAutomator.cs"),
-		GD.Load<Texture2D>("res://addons/Rail2DAutomator/Nodes/RailAutomator//icon.png"));
-		AddCustomType("AutoRailNode", "Node2D", GD.Load<Script>("res://addons/Rail2DAutomator/Nodes/AutoRailNode/AutoRailNode.cs"),
-		GD.Load<Texture2D>("res://addons/Rail2DAutomator/Nodes/AutoRailNode//icon.png"));
-        AddAutoloadSingleton("RailAutomator", "res://addons/Rail2DAutomator/Scenes/AutomatorSingleton.tscn");
 		// Initialization of the plugin goes here.
 	}
 
